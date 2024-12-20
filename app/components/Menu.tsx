9f1a9de3d90bb51932f522984841773de9a71bc0
Lucas Pinheiro Machado <lucaslpma10@gmail.com>
2024-12-20 12:52:03 -0300

    mensagem de obs

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -9,6 +9,7 @@ export default function Menu({ onStart }: MenuProps) {
     <div className="text-center">
       <h1 className="text-4xl font-bold mb-4">Tiro ao alvo</h1>
       <p className="mb-4">Defenda sua posição, não deixe os alvos chegarem até você!</p>
+      <p className="obs">(obs: para jogar você precisa de um mouse conectado e o navegador deve estar no modo ´´para computador``)</p>
       <Button onClick={onStart}>Iniciar Jogo</Button>
     </div>
   )
